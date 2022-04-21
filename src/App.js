da8e383064ecfceeb2b41640a1a5f0b9cbafe21e
realericquach <kevinpoispokevin@gmail.com>
2022-04-21 13:46:35 -0500

    updating

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import * as GrIcons from "react-icons/gr"
+import eric from './images/eric.jpg'
+import { Document, Page } from 'react-pdf';
+
+import resume from `/images/Eric Quach Resume.pdf`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="home">
+      <Documnet file={resume}>
+        <Page pageNumber={(1)}></Page>
+      </Documnet>
     </div>
   );
 }
+
+/*<div className="profile">
+        <div className="left">
+          <div>
+            <img src={eric}></img>
+          </div>
+          <h1>Eric Quach</h1>
+          <div className="title"><h3>Junior Front End Developer</h3></div>
+          <h3>January 11th, 1999</h3>
+          <h3>St. Louis, MO</h3>
+          <h3>realericquach@gmail.com</h3>
+        </div>
+        <div className="right">
+          <p>Work In Progress</p>
+        </div>
+      </div>
+      <footer>
+        <div></div>
+      </footer>*/
 
 export default App;
